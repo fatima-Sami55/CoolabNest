8bcddfd4e6729c1d5ffb34016c200ab8db00bcea
FatimaSami <84532632+fatima-Sami55@users.noreply.github.com>
2025-06-21 02:17:06 +0500

    Initial commit for CollabNest �

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
